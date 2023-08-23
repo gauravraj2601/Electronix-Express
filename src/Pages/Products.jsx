8f1901afeccb968ef71bd18a52d81c37a86f4fdb
gauravraj2601 <gauravraj2601@gmail.com>
2023-08-23 13:37:07 +0530

    package

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import { styled } from 'styled-components';
 const Products = () => {
   return (
-    <div>Products</div>
+    <DIV>
+        <div className='sidebar'>sidebar</div>
+        <div className='card_container'>product container</div>
+    </DIV>
   )
 }
 
 export default Products
+
+const DIV= styled.div`
+    display: flex;
+    .sidebar{
+        width:20%
+    }
+`
